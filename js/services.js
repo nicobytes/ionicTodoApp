b3c58bca39b194b54816386eea084b2aecb27e3b
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2015-09-15 03:47:55 -0500

    Crear servicio

--- js/services.js
+++ js/services.js
@@ -1,35 +1,60 @@
 angular.module('starter.services', [])
 
-.factory('todoStorage', function() {
-  var STORAGE_ID = 'todos';
+.factory('todoService', function() {
+  var todos = [
+    {
+      id: 1,
+      title: 'Hacer las compras',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis nesciunt repellendus deserunt pariatur quidem ab nobis, obcaecati ratione eos cumque et unde quam doloribus consectetur reprehenderit! Tenetur maxime voluptates molestiae?',
+      date: new Date(2015, 8, 4, 7),
+      done: true
+    },
+    {
+      id: 2,
+      title: 'Leer una novela',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis nesciunt repellendus deserunt pariatur quidem ab nobis, obcaecati ratione eos cumque et unde quam doloribus consectetur reprehenderit! Tenetur maxime voluptates molestiae?',
+      date: new Date(2015, 9, 4, 7),
+      done: false
+    },
+    {
+      id: 3,
+      title: 'Comprar comida',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis nesciunt repellendus deserunt pariatur quidem ab nobis, obcaecati ratione eos cumque et unde quam doloribus consectetur reprehenderit! Tenetur maxime voluptates molestiae?',
+      date: new Date(),
+      done: true
+    },
+    {
+      id: 4,
+      title: 'Ir a meetup',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis nesciunt repellendus deserunt pariatur quidem ab nobis, obcaecati ratione eos cumque et unde quam doloribus consectetur reprehenderit! Tenetur maxime voluptates molestiae?',
+      date: new Date(),
+      done: true
+    },
+    {
+      id: 5,
+      title: 'Sacar a la mascota',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis nesciunt repellendus deserunt pariatur quidem ab nobis, obcaecati ratione eos cumque et unde quam doloribus consectetur reprehenderit! Tenetur maxime voluptates molestiae?',
+      date: new Date(2015, 8, 4, 9),
+      done: false
+    },
+  ]
   return {
-    query: function () {
-      return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-    },
-    save: function (todos) {
-      localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-    },
-    addTodo: function(todo){
-      var newTodo = {
-        name: todo.name,
-        text: todo.text,
-        date: new Date(todo.date),
-        done: todo.done || false
-      };
-      var todos = this.query();
-      todos.push(newTodo);
-      this.save(todos);
+    getAll: function () {
       return todos;
     },
-    deleteTodo: function(todo){
-      var todos = this.query();
+    create: function(todo){
+      var todos = this.getAll();
+      todos.push(todos);
+      return todos;
+    },
+    delete: function(todo){
+      var todos = this.getAll();
       var index = todos.indexOf(todo);
       todos.splice(index, 1);
-      this.save(todos);
       return todos;
     },
-    groupByDate: function(){
-      var todos = this.query().map(function(item){
+    getGroupDate: function(){
+      var todos = this.getAll().map(function(item){
         var date = new Date(item.date);
         item.shortDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
         return item;
@@ -41,13 +66,5 @@ angular.module('starter.services', [])
         };
       });
     },
-    saveDates: function(dates){
-      var todos = dates.map(function(item){
-        return item.todos;
-      }).reduce(function(a,b){
-        return a.concat(b);
-      }, []);
-      this.save(todos);
-    }
   };
 });
